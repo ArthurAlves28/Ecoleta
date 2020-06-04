c6302293f04130f2cb394d9db7208716cb9d857d
ArthurAlves28 <arthuralvessa@gmail.com>
2020-06-04 16:21:19 -0300

    Aula 2 finalizada: Back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 
-// As rotas serão desacopladas do arquivo da aplicação
-const  routes = express.Router();
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get('/', (request, response) => {
-    return response.json({message:'Hello world'});
-});
+// As rotas serão desacopladas do arquivo da aplicação
+// Padrão para o nome dos métodos das rotas
+// index: para listar items
+// show: para mostrar um item
+// create: para adicionar ao banco de dados
+// updade: para atualizar o banco de dados
+// delede: para excluir o registro
+
+const  routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 // Exportar as rotas para utilizar na aplicação
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
-import express, { request, response } from "express";
+import express from "express";
+import cors from 'cors';
+import path from 'path';
 import routes from "./routes"
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes); // A aplicação vai utilizar as rotas do arquivo routes.ts
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uplodas')));
 
 app.listen(3333);
