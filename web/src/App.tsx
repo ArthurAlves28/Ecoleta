1f3554f1a1beb1b07000cdd36d77916d5306061e
ArthurAlves28 <arthuralvessa@gmail.com>
2020-06-08 20:02:43 -0300

    Tela principal criada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// JSX: Sintaxe de XML dentro do JavaScript
-
-import Header from './Header';
+import Home from './pages/Home';
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
   return (
-    <div>
-      <Header title={`Contador: ${counter}`}/>
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    //<Home />
+    <Routes />
   );
 }
 
