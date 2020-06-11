dfdae2b0fa8851c83253c6af3e568fa3495cc9d5
ArthurAlves28 <arthuralvessa@gmail.com>
2020-06-11 13:06:17 -0300

    Cadastro de ponto de coleta na API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -29,11 +29,20 @@ const CreatePoint = () => {
     const [ufs, setUfs] = useState<string[]>([]);
     const [cities, setCities] = useState<string[]>([]);
 
-    const[initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: '',
+    });
 
     const [selectedUF, setSelectedUF] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition( position => {
@@ -90,6 +99,51 @@ function handleMapClick(event: LeafletMouseEvent) {
     ])
 }
 
+function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+}
+
+function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => item !== id)
+
+        setSelectedItems(filteredItems);
+    } else {
+        setSelectedItems( [...selectedItems, id] );
+    }
+}
+
+async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUF;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+        name,
+        email,
+        whatsapp,
+        uf,
+        city,
+        latitude,
+        longitude,
+        items
+    }
+
+    await api.post('points', data);
+
+    alert('Ponto de coleta criado!');
+
+    history.push('/');
+}
+
     return (
         <div id="page-create-point">
             <header>
@@ -101,7 +155,7 @@ function handleMapClick(event: LeafletMouseEvent) {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
 
                 <fieldset>
@@ -112,9 +166,10 @@ function handleMapClick(event: LeafletMouseEvent) {
                 <div className="field">
                     <label htmlFor="name">Nome da entidade</label>
                     <input 
-                    type="text"
-                    name="name"
-                    id="name"
+                        type="text"
+                        name="name"
+                        id="name"
+                        onChange={handleInputChange}
                     />
                 </div>
                 <div className="field-group">
@@ -124,6 +179,7 @@ function handleMapClick(event: LeafletMouseEvent) {
                             type="email"
                             name="email"
                             id="email"
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="field">
@@ -132,6 +188,7 @@ function handleMapClick(event: LeafletMouseEvent) {
                             type="text"
                             name="whatsapp"
                             id="whatsapp"
+                            onChange={handleInputChange}
                         />
                     </div>
                 </div>
@@ -191,7 +248,11 @@ function handleMapClick(event: LeafletMouseEvent) {
 
                 <ul className="items-grid">
                     {itmes.map(item => (
-                        <li key={item.id}>
+                        <li 
+                            key={item.id} 
+                            onClick={() => handleSelectItem(item.id)}
+                            className={selectedItems.includes(item.id) ? 'selected' : ''}
+                        >
                             <img src={item.image_url} alt={item.title} />
                             <span>{item.title}</span>
                         </li>
